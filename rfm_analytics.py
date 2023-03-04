40f9fc02be8a7d9990031fb1cf89309da8366279
Ugur Saricam <105130977+ugursaricam@users.noreply.github.com>
2023-03-05 00:00:52 +0300

    Update rfm_analytics.py

--- rfm_analytics.py
+++ rfm_analytics.py
@@ -32,9 +32,6 @@
 ##########################################
 # 2. Understanding the Data
 ##########################################
-import warnings
-
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import pandas as pd
 import numpy as np
